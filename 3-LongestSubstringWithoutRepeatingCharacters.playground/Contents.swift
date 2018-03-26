6448be505f39d9fe9dbe439d47ad34fe296c8cda
Patrick Gao <patrickgao0922@gmail.com>
2018-03-26 14:50:37 +1100

    start number 7

--- 3-LongestSubstringWithoutRepeatingCharacters.playground/Contents.swift
+++ 3-LongestSubstringWithoutRepeatingCharacters.playground/Contents.swift
@@ -2,8 +2,8 @@
 // 3. Longest Substring Without Repeating Characters: https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
 
 import Foundation
-let str = "WERTYUIOP"
-class Solution {
+var str = "fdsdfas"
+class OldSolution {
     func lengthOfLongestSubstring(_ s: String) -> Int {
         for subStrLen in stride(from: s.count, to: 1, by: -1) {
             for index in 0...s.count-subStrLen {
@@ -14,7 +14,10 @@ class Solution {
                 }
             }
         }
-        return 1
+        if s.count != 0 {
+            return 1
+        }
+        return 0
     }
     
     func getSubString(forString string:String, from:Int, to:Int) -> String {
@@ -25,7 +28,44 @@ class Solution {
     }
 }
 
+/// Sliding Window
+class Solution {
+    func lengthOfLongestSubstring(_ s: String) -> Int {
+        if s.count <= 1 {
+            return s.count
+        }
+        var maxNum = 0
+        var i = 0
+        var j = 0
+        //        [i,j] Whindow
+        //        var set = Set<Character>()
+        var dic = [Character:Int]()
+        while (i < s.count && j < s.count) {
+            var iIndex = s.index(s.startIndex, offsetBy: i)
+            var jIndex = s.index(s.startIndex, offsetBy: j)
+            if let charIndex = dic[s[jIndex]] {
+                //                set.insert(s[jIndex])
+                i = max(charIndex, i)
+            }
+            
+            j += 1
+            dic[s[jIndex]] = j
+            
+            maxNum = max(maxNum,j-i )
+            
+            
+            
+            
+        }
+        
+        return maxNum
+    }
+}
+
 let solution = Solution()
 solution.lengthOfLongestSubstring(str)
+
+//let oldSolution = OldSolution()
+//oldSolution.lengthOfLongestSubstring(str)
 
 
--- 5-LongestPalindromicSubstring.playground/Contents.swift
+++ 5-LongestPalindromicSubstring.playground/Contents.swift
@@ -3,12 +3,15 @@
 
 import Foundation
 
-class Solution {
+class OldSolution {
     func longestPalindrome(_ s: String) -> String {
-        for subStrLen in stride(from: s.count, to: 1, by: -1) {
+        if s.count == 1 {
+            return s
+        }
+        for subStrLen in stride(from: s.count, to: 0, by: -1) {
             for index in 0...s.count-subStrLen {
                 let subString = getSubString(forString: s, from: index, to: index+subStrLen)
-                if subString.first == subString.last {
+                if isPalindromic(subString) {
                     return subString
                 }
             }
@@ -22,8 +25,75 @@ class Solution {
         let endIndex = str.index(str.startIndex, offsetBy: to)
         return String(str[startIndex..<endIndex])
     }
+    
+    func isPalindromic(_ s:String) -> Bool {
+        if s.count == 1 {
+            return true
+        }
+        let compareLen = Int(ceil(Double(s.count)/2.0))
+        for i in 0...(compareLen) {
+            let firstIndex = s.index(s.startIndex, offsetBy: i)
+            let secondIndex = s.index(s.startIndex, offsetBy: s.count-i-1)
+            s[firstIndex]
+            s[secondIndex]
+            if s[firstIndex] != s[secondIndex] {
+                return false
+            }
+        }
+        return true
+        
+    }
 }
 
 
+//let oldSolution = OldSolution()
+//oldSolution.longestPalindrome("ci")
+
+
+class Solution {
+    func longestPalindrome(_ s: String) -> String {
+        var start = 0
+        var end = 0
+        // Move center
+        for i in 0..<s.count {
+            var len = 0
+            // expand from center
+            let subStr1Len = expandFromCenter(string: s, leftCenter: i, rightCenter: i)
+            let subStr2Len = expandFromCenter(string: s, leftCenter: i, rightCenter: i+1)
+            len = max(subStr1Len,subStr2Len)
+            if (len > end-start+1) {
+                start = i - (len-1)/2
+                end = i+len/2
+            }
+        }
+        print(start)
+        print(end)
+        let startIndex = s.index(s.startIndex, offsetBy: start)
+        let endIndex = s.index(s.startIndex, offsetBy: end)
+        return String(s[startIndex...endIndex])
+    }
+    
+    func expandFromCenter(string:String, leftCenter:Int, rightCenter:Int) -> Int {
+        var leftPo = leftCenter
+        var rightPo = rightCenter
+        var subStr = ""
+        while (leftPo >= 0 && rightPo < string.count) {
+            var leftIndex = string.index(string.startIndex, offsetBy: leftPo)
+            var rightIndex = string.index(string.startIndex, offsetBy: rightPo)
+            if (string[leftIndex]) == (string[rightIndex]) {
+                leftPo -= 1
+                rightPo += 1
+            } else {
+                return rightPo - leftPo - 1
+            }
+            
+        }
+        
+        return rightPo - leftPo - 1
+    }
+    
+}
 let solution = Solution()
-solution.longestPalindrome("abadbbbbbaaaaaaaaaaaaaba")
+solution.longestPalindrome("efaaaab")
+
+

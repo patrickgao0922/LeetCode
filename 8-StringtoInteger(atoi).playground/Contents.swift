8f6470c6d81bff0458baf1755e867d6c58622235
Patrick Gao <patrickgao0922@gmail.com>
2018-03-26 20:10:11 +1100

    commit

--- 8-StringtoInteger(atoi).playground/Contents.swift
+++ 8-StringtoInteger(atoi).playground/Contents.swift
@@ -12,19 +12,70 @@
 //
 //If no valid conversion could be performed, a zero value is returned. If the correct value is out of the range of representable values, INT_MAX (2147483647) or INT_MIN (-2147483648) is returned.
 import UIKit
-
-var str = "Hello, playground"
+Int32.min
+var str = "-2147483649"
 
 class Solution {
     func myAtoi(_ str: String) -> Int {
         var string = str
-        return 0
         
-        func discardsBeginningWhitespace(){
+        var result = ""
+        string = discardsBeginningWhitespace(string:string)
+        if string.count == 0 {
+            return 0
+        }
+        if string[string.startIndex] == Character("-") || string[string.startIndex] == Character("+") {
+            result.append(string[string.startIndex])
+            string.remove(at: string.startIndex)
+        }
+        if string.count == 0 || string[string.startIndex] < Character("0") || string[string.startIndex] > Character("9") {
+            return 0
+        }
+        while (string.count != 0 && string[string.startIndex] >= Character("0") && string[string.startIndex] <= Character("9")) {
+            result.append(string[string.startIndex])
+            string.remove(at: string.startIndex)
+        }
+        
+        // Change negative bound
+        if result[result.startIndex] == Character("-") {
+            var minValueString = String(Int32.min)
+            if result.count > minValueString.count {
+                return Int(Int32.min)
+            }
+        } else {
             
-            while string[string.startIndex] != Character(" ") {
-                string.remove(at: string.startIndex)
+            if result[result.startIndex] == Character("+") {
+                result.remove(at: result.startIndex)
+            }
+            
+            var maxValueString = String(Int32.max)
+            if result.count > maxValueString.count {
+                return Int(Int32.max)
             }
         }
+        
+        guard let value = Int(result) else{
+            return 0
+        }
+        if value < Int32.min {
+            return Int(Int32.min)
+        } else if value > Int32.max{
+            return Int(Int32.max)
+        } else {
+            return value
+        }
+        return 0
+        
+    }
+    func discardsBeginningWhitespace(string:String) -> String{
+        var tempString = string
+        while tempString.count != 0 && tempString[tempString.startIndex] == Character(" "){
+            tempString.remove(at: tempString.startIndex)
+        }
+        return tempString
     }
 }
+
+let soluton = Solution()
+soluton.myAtoi(str)
+

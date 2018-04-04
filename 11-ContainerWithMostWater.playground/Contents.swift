86d191dfadbbb0c83ed318187d69ce7ade7b70f6
Patrick Gao <patrickgao0922@gmail.com>
2018-04-04 13:14:53 +1000

    commit

--- 11-ContainerWithMostWater.playground/Contents.swift
+++ 11-ContainerWithMostWater.playground/Contents.swift
@@ -3,5 +3,30 @@
 //https://leetcode.com/problems/container-with-most-water/description/
 
 import UIKit
+let num = [4,8,2,6,3,8,3,6]
+class Solution {
+    func maxArea(_ height: [Int]) -> Int {
+        
+        var leftIndex = 0
+        var rightIndex = height.count - 1
+        
+        var max = abs(height[leftIndex] - height[rightIndex]) * min(height[leftIndex], height[rightIndex])
+        
+        while leftIndex != rightIndex {
+            print(max)
+            let tempDiff = abs(leftIndex - rightIndex) * min(height[leftIndex], height[rightIndex])
+            if (max < tempDiff) {
+                max = tempDiff
+            }
+            if height[leftIndex] < height[rightIndex] {
+                leftIndex += 1
+            } else {
+                rightIndex -= 1
+            }
+        }
+        return max
+    }
+}
 
-var str = "Hello, playground"
+let solution = Solution()
+let result = solution.maxArea(num)
